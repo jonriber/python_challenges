dfc07cdd8aedfa5ca9ebdaec03b90ba9aa8b802f
jonriber <49167364+jonriber@users.noreply.github.com>
2020-05-25 21:34:42 -0300

    update sort_a_string

--- python_challenges/sort_a_string/sort_a_string.py
+++ python_challenges/sort_a_string/sort_a_string.py
@@ -2,17 +2,24 @@
 
 def sort_a_string(object):
     if type(object) == str:
-        new_string_list = list(object.split(' '))
-        return sorted(new_string_list)
+        string_split = object.split(' ')
+        duplicate_words = [word.lower()+word for word in string_split]
+        sort_duplicate_words = sorted(duplicate_words)
+        final_list = [word[len(word)//2:] for word in sort_duplicate_words]
+        result = ' '.join(final_list)
+        return result
     else:
         print('nao é string')
-        new_string_list_formatted = list(str(object))
-        return sorted(new_string_list_formatted)
+        
 
 if __name__ == '__main__':
    # x = sorted(sort_a_string('o esse teste'))
    #print(sort_a_string('o esse teste'))
-   print(sort_a_string(20))
+   print(sort_a_string('o a bola esse teste'))
+   print(sort_a_string('teste'))
+   print(sort_a_string('teste BANANA apple'))
+
+
    # print(x)
    # final = ','.join(x)
     #print(final)
