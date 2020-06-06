d7a45543cae2f04e157680cb52e198ae6f0444ea
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-05 23:58:19 -0300

    Atualização de find_all_list_items e teste

--- python_challenges/find_all_list_items/find_all_list_items.py
+++ python_challenges/find_all_list_items/find_all_list_items.py
@@ -1,4 +1,16 @@
-
+# Função de pesquisa de um elemento em uma lista dada.
+# O resultado é uma lista com o índice do elemento pesquisado dentro da lista de pesquisa;
 
 def find_list_items(a_list):
     return a_list
+
+def index_func(search_list,item):
+    result = []
+    for i in range(len(search_list)):
+        if search_list[i] == item:
+            result.append([i])
+        elif isinstance(search_list[i],list):
+            for index in index_func(search_list[i],item):
+                result.append([i]+index)
+        
+    return result
--- python_challenges/find_all_list_items/test_function.py
+++ python_challenges/find_all_list_items/test_function.py
@@ -1,6 +1,24 @@
-from find_all_list_items import find_list_items
+# Utilizando pytest para realizar TDD e tentar quebrar o código
+
+from find_all_list_items import find_list_items,index_func
 
 
 def test_func_returning_parameters():
     example = [1,2,3]
     assert find_list_items(example) == [1,2,3]
+
+def test_index_func_simple_1():
+    search_list = [[[1,2,3],2,[1,3]],[1,2,3]]
+    item = 2
+
+    assert index_func(search_list,item) == [[0,0,1],[0,1],[1,1]]
+
+def test_index_func_simple_2():
+    search_list = [1,2,3,2,4,2]
+    item = 2
+    assert index_func(search_list,item) == [[1],[3],[5]]
+
+def test_index_func_simple_3():
+    search_list = [2]
+    item = 2
+    assert index_func(search_list,item)==[[0]]
