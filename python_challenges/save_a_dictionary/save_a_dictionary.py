74cab0e4f968b6515b2fe3321994834ebea14fbc
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-07 21:26:46 -0300

    functions

---
+++ python_challenges/save_a_dictionary/save_a_dictionary.py
@@ -0,0 +1,8 @@
+# O objetivo é criar uma função para salvar um dicionário em um arquivo externo
+# Então criar outra função para recuperar esse dicionário através deste arquivo
+
+def save_content(dictionary,file_path):
+    pass
+
+def retrieve_content(file_path):
+    pass
---
+++ python_challenges/save_a_dictionary/test_save_a_dictionary.py
@@ -0,0 +1,1 @@
+# test unitário utilizando pytest
