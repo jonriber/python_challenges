0c182066c69df22d60a475d76ad1cbc5ad6d5086
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-17 18:27:51 -0300

    count_unique_words update

---
+++ python_challenges/cout_unique_words/count_unique_words.py
@@ -0,0 +1,23 @@
+import re
+from collections import Counter
+
+def count_words(path):
+    with open(path,encoding='utf-8') as file:
+        all_words = re.findall(r"[0-9a-zZ-Z-']+",file.read())
+        all_words = [word.upper() for word in all_words]
+        print('\nTotal Words:',len(all_words))
+
+        word_counts = Counter()
+        for word in all_words:
+            word_counts[word] +=1
+
+        print('\nTop 20 Words:')
+        for word in word_counts.most_common(20):
+            print(word[0],'\t',word[1])
+
+if __name__== '__main__':
+    path1 = r"C:\Users\jonat\Documents\PROGRAMAÇÃO\Python\Challenges\python_challenges\python_challenges\cout_unique_words\100-0.txt"
+    path2 = r"C:\Users\jonat\Documents\PROGRAMAÇÃO\Python\Challenges\python_challenges\python_challenges\cout_unique_words\first_text.txt"
+    print('Iniciando programa de contagem de palavras.')
+    count_words(path1)
+    count_words(path2)
---
+++ python_challenges/cout_unique_words/test_count_unique_words.py
@@ -0,0 +1,9 @@
+from . import count_unique_words
+
+class TestCase:
+
+    def test_1(self):
+        # path = r"C:\Users\jonat\Documents\PROGRAMAÇÃO\Python\Challenges\python_challenges\python_challenges\cout_unique_words\first_text.txt"
+        
+        # assert count_words(path) ==
+        pass
